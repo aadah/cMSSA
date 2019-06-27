9dcf5a3a3aacdd5f23e21e59bb7eca50237729e0
Abdi-Hakin Dirie <abdidirie0@gmail.com>
2019-06-27 01:48:47 -0400

    sort by magnitude than by order

--- cMSSA/ssa.py
+++ cMSSA/ssa.py
@@ -178,7 +178,7 @@ class CMSSA(object):
         if alpha > 0:
             C -= alpha * self.C_bg
         self.eig_vals, self.eig_vecs = linalg.eig(C)
-        idxs = np.argsort(-self.eig_vals)
+        idxs = np.argsort(-abs(self.eig_vals))
         E = self.eig_vecs[:, idxs[:self.num_comp]]
         e = self.eig_vals[idxs[:self.num_comp]]
         return E, e
